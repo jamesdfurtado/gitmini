1b637517ff6c164dd0ea2ba05cc55b6cb0d5b1a5
James Furtado <jamesdfurtado@gmail.com>
2025-06-03 17:57:01 -0400

    Implemented 'gitmini init' functionality

--- gitmini/commands/init.py
+++ gitmini/commands/init.py
@@ -1,2 +1,17 @@
+import os
+
 def handle_init(args):
-    print('Init not implemented yet')
+    gitmini_dir = '.gitmini'
+    subdirs = ['objects', 'logs']
+    index_file = 'index'
+
+    if os.path.exists(gitmini_dir):
+        print("GitMini repository already initialized.")
+        return
+
+    os.makedirs(gitmini_dir)
+    for sub in subdirs:
+        os.makedirs(os.path.join(gitmini_dir, sub))
+    open(os.path.join(gitmini_dir, index_file), 'w').close()
+
+    print("Initialized empty GitMini repository in .gitmini/")
