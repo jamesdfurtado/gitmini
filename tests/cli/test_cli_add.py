46a1fb4d4024718f8c045bde1dc6027e06a3d619
James Furtado <jamesdfurtado@gmail.com>
2025-06-04 15:13:04 -0400

    Adjusted test folders for cleaner test running practices; added new add function test cases

--- tests/cli/test_cli_add.py
+++ tests/cli/test_cli_add.py
@@ -1,37 +1,29 @@
 import os
-import shutil
-import subprocess
 from tests.test_helpers import GitMiniTestCase, GITMINI_DIR
-
 
 class TestAddCommand(GitMiniTestCase):
 
     def test_cli_add_without_repo(self):
         """ Test that 'gitmini add' fails when .gitmini is missing """
-        result = subprocess.run(
-            ['python', '-m', 'gitmini', 'add'],
-            capture_output=True,
-            text=True
-        )
+        result = self.run_gitmini(['add'])
         self.assertIn("fatal: not a gitmini repository", result.stderr)
 
     def test_cli_add_with_repo(self):
         """ Test that 'gitmini add' finds the .gitmini root and prints success message """
-        subprocess.run(['python', '-m', 'gitmini', 'init'], capture_output=True, text=True)
-
-        result = subprocess.run(
-            ['python', '-m', 'gitmini', 'add'],
-            capture_output=True,
-            text=True
-        )
+        self.run_gitmini(['init'])
+        result = self.run_gitmini(['add'])
         self.assertIn("Located .gitmini repo at:", result.stdout)
 
     def test_cli_add_help(self):
         """ Test that 'gitmini add --help' displays the help message """
-        result = subprocess.run(
-            ['python', '-m', 'gitmini', 'add', '--help'],
-            capture_output=True,
-            text=True
-        )
+        result = self.run_gitmini(['add', '--help'])
         self.assertIn("usage: gitmini add", result.stdout)
         self.assertIn("Add specified files to GitMini's staging area for the next commit.", result.stdout)
+
+    def test_cli_add_specific_file_and_folder(self):
+        """ Test that 'gitmini add <file.txt> <folder>' collects the correct files """
+        self.run_gitmini(['init'])
+        result = self.run_gitmini(['add', 'file.txt', 'folder'])
+
+        self.assertIn("file.txt", result.stdout)
+        self.assertIn("folder\\infolder.txt", result.stdout)
--- tests/cli/test_cli_init.py
+++ tests/cli/test_cli_init.py
@@ -1,18 +1,12 @@
 import os
-import shutil
-import subprocess
 from tests.test_helpers import GitMiniTestCase, GITMINI_DIR
-
 
 class TestInitCommand(GitMiniTestCase):
 
     def test_cli_init_stub(self):
-        """ Test that 'gitmini init' initializes the correct folder structure and prints confirmation """
-        result = subprocess.run(
-            ['python', '-m', 'gitmini', 'init'],
-            capture_output=True,
-            text=True
-        )
+        """ Test that 'gitmini init' initializes .gitmini repo correctly """
+        result = self.run_gitmini(['init'])
+
         self.assertIn("Initialized empty GitMini repository in .gitmini/", result.stdout)
         self.assertTrue(os.path.isdir(GITMINI_DIR))
         self.assertTrue(os.path.isdir(os.path.join(GITMINI_DIR, 'objects')))
@@ -21,22 +15,12 @@ class TestInitCommand(GitMiniTestCase):
 
     def test_cli_init_already_initialized(self):
         """ Test that 'gitmini init' does not reinitialize an existing repository """
-
-        subprocess.run(['python', '-m', 'gitmini', 'init'], capture_output=True, text=True)
-        result = subprocess.run(
-            ['python', '-m', 'gitmini', 'init'],
-            capture_output=True,
-            text=True
-        )
+        self.run_gitmini(['init'])
+        result = self.run_gitmini(['init'])
         self.assertIn("GitMini repository already initialized.", result.stdout)
 
     def test_cli_init_help(self):
-        """ Test that 'gitmini init --help' displays the help message """
-
-        result = subprocess.run(
-            ['python', '-m', 'gitmini', 'init', '--help'],
-            capture_output=True,
-            text=True
-        )
+        """ Test that 'gitmini init --help' shows help msg """
+        result = self.run_gitmini(['init', '--help'])
         self.assertIn("usage: gitmini init", result.stdout)
         self.assertIn("Initialize a new GitMini repository in the current directory.", result.stdout)
